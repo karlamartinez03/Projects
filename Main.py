12e99594ff9b35feb16bac29e7023c83432957d5
karlamartinez03 <kaitaely@gmail.com>
2024-06-17 00:25:43 -0500

    created main display menu and add transaction function

--- Main.py
+++ Main.py
@@ -1,1 +1,27 @@
 transactions = []
+
+def display_menu():
+    print("\nPersonal Budget Tracker")
+    print("1. Add Income")
+    print("2. Add Expense")
+    print("3. View Summary")
+    print("4. View Transactions")
+    print("5. Exit")
+
+
+
+def add_transaction(is_income):
+    description = input("Enter description: ")
+    try:
+        amount = float(input("Enter amount: "))
+    except ValueError:
+        print("invalid amount. Please enter a number.")
+        return 
+    category = input("Enter category: ")
+    transaction = {
+        "description": description,
+        "amount": amount if is_income else -amount, 
+        "category": category
+    }
+    transactions.append(transaction)
+
